2c22f395955edb23fb0eeb1f2438e03c36fff676
Colin <colin.p.h.michel@gmail.com>
2025-03-17 00:18:09 +0100

    Colin: Fixed the error when dynamically displaying the projects in a grid. We should be good to go with adding all the content now. Most functionalities have been tested.

--- javascript/projects-data.js
+++ javascript/projects-data.js
@@ -20,21 +20,81 @@ const projectsData = [
         categories: ["all", "home-page"]
     },
     {
-        title: "FRAMEWORK",
-        imageAlt: "FRAMEWORK project thumbnail.",
-        thumbnail: "projects/FRAMEWORK/images/Thumbnail_FRAMEWORK.jpg",
+        title: "STRIBE",
+        imageAlt: "STRIBE project thumbnail.",
+        thumbnail: "projects/STRIBE/images/Thumbnail_STRIBE.png",
         description: "Description of Project A",
-        link: "projects/Project_FRAMEWORK.html",
+        link: "projects/Project_STRIBE.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
     },
     {
-        title: "FRAMEWORK",
-        imageAlt: "FRAMEWORK project thumbnail.",
-        thumbnail: "projects/FRAMEWORK/images/Thumbnail_FRAMEWORK.jpg",
+        title: "SERVER",
+        imageAlt: "SERVER project thumbnail.",
+        thumbnail: "projects/SERVER/images/Thumbnail_SERVER.jpg",
         description: "Description of Project A",
-        link: "projects/Project_FRAMEWORK.html",
+        link: "projects/Project_SERVER.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "DELVE",
+        imageAlt: "DELVE project thumbnail.",
+        thumbnail: "projects/DELVE/images/Thumbnail_DELVE.jpg",
+        description: "Description of Project A",
+        link: "projects/Project_DELVE.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "TOOLS",
+        imageAlt: "TOOLS project thumbnail.",
+        thumbnail: "projects/TOOLS/images/Thumbnail_TOOLS.png",
+        description: "Description of Project A",
+        link: "projects/Project_TOOLS.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "FISHTON",
+        imageAlt: "FISHTON project thumbnail.",
+        thumbnail: "projects/FISHTON/images/Thumbnail_FISHTON.png",
+        description: "Description of Project A",
+        link: "projects/Project_FISHTON.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "SANDSTORM",
+        imageAlt: "SANDSTORM project thumbnail.",
+        thumbnail: "projects/SANDSTORM/images/Thumbnail_SANDSTORM.png",
+        description: "Description of Project A",
+        link: "projects/Project_SANDSTORM.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "LRRP",
+        imageAlt: "LRRP project thumbnail.",
+        thumbnail: "projects/LRRP/images/Thumbnail_LRRP.png",
+        description: "Description of Project A",
+        link: "projects/Project_LRRP.html",
+        roleTags: ["Programmer"],
+        competenceTags: ["Unity", "C#", "Verdaccio"],
+        categories: ["all", "home-page"]
+    },
+    {
+        title: "TEMPUS",
+        imageAlt: "TEMPUS project thumbnail.",
+        thumbnail: "projects/TEMPUS/images/Thumbnail_TEMPUS.png",
+        description: "Description of Project A",
+        link: "projects/Project_TEMPUS.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
