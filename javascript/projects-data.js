12b9c77843b95d1bf6717093995f2bd52c39a332
Colin <colin.p.h.michel@gmail.com>
2025-03-17 10:30:07 +0100

    Colin: Updated the project pages and linked them to the project cards within the "projects" page.

--- javascript/projects-data.js
+++ javascript/projects-data.js
@@ -3,8 +3,9 @@ const projectsData = [
         title: "Wickie & The Soul Keeper",
         imageAlt: "Wickie project thumbnail.",
         thumbnail: "projects/WICKIE/images/Thumbnail_WICKIE.png",
-        description: "DADIU project of 2022. A visual and contemplative puzzle game. Made with a multidisciplinary team of 17 over 10 weeks.",
-        link: "projects/Project_WICKIE.html",
+        description: "DADIU project of 2022. A visual and contemplative puzzle game. " +
+            "Made with a multidisciplinary team of 17 over 10 weeks.",
+        link: "projects/WICKIE/Project_WICKIE.html",
         roleTags: ["Producer"],
         competenceTags: ["collaboration", "Management"],
         categories: ["all", "home-page"]
@@ -14,7 +15,7 @@ const projectsData = [
         imageAlt: "FRAMEWORK project thumbnail.",
         thumbnail: "projects/FRAMEWORK/images/Thumbnail_FRAMEWORK.jpg",
         description: "Description of Project A",
-        link: "projects/Project_FRAMEWORK.html",
+        link: "projects/FRAMEWORK/Project_FRAMEWORK.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -24,7 +25,7 @@ const projectsData = [
         imageAlt: "STRIBE project thumbnail.",
         thumbnail: "projects/STRIBE/images/Thumbnail_STRIBE.png",
         description: "Description of Project A",
-        link: "projects/Project_STRIBE.html",
+        link: "projects/STRIBE/Project_STRIBE.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -34,7 +35,7 @@ const projectsData = [
         imageAlt: "SERVER project thumbnail.",
         thumbnail: "projects/SERVER/images/Thumbnail_SERVER.jpg",
         description: "Description of Project A",
-        link: "projects/Project_SERVER.html",
+        link: "projects/SERVER/Project_SERVER.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -44,7 +45,7 @@ const projectsData = [
         imageAlt: "DELVE project thumbnail.",
         thumbnail: "projects/DELVE/images/Thumbnail_DELVE.jpg",
         description: "Description of Project A",
-        link: "projects/Project_DELVE.html",
+        link: "projects/DELVE/Project_DELVE.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -54,7 +55,7 @@ const projectsData = [
         imageAlt: "TOOLS project thumbnail.",
         thumbnail: "projects/TOOLS/images/Thumbnail_TOOLS.png",
         description: "Description of Project A",
-        link: "projects/Project_TOOLS.html",
+        link: "projects/TOOLS/Project_TOOLS.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -64,7 +65,7 @@ const projectsData = [
         imageAlt: "FISHTON project thumbnail.",
         thumbnail: "projects/FISHTON/images/Thumbnail_FISHTON.png",
         description: "Description of Project A",
-        link: "projects/Project_FISHTON.html",
+        link: "projects/FISHTON/Project_FISHTON.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -74,7 +75,7 @@ const projectsData = [
         imageAlt: "SANDSTORM project thumbnail.",
         thumbnail: "projects/SANDSTORM/images/Thumbnail_SANDSTORM.png",
         description: "Description of Project A",
-        link: "projects/Project_SANDSTORM.html",
+        link: "projects/SANDSTORM/Project_SANDSTORM.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -84,7 +85,7 @@ const projectsData = [
         imageAlt: "LRRP project thumbnail.",
         thumbnail: "projects/LRRP/images/Thumbnail_LRRP.png",
         description: "Description of Project A",
-        link: "projects/Project_LRRP.html",
+        link: "projects/LRRP/Project_LRRP.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
@@ -94,7 +95,7 @@ const projectsData = [
         imageAlt: "TEMPUS project thumbnail.",
         thumbnail: "projects/TEMPUS/images/Thumbnail_TEMPUS.png",
         description: "Description of Project A",
-        link: "projects/Project_TEMPUS.html",
+        link: "projects/TEMPUS/Project_TEMPUS.html",
         roleTags: ["Programmer"],
         competenceTags: ["Unity", "C#", "Verdaccio"],
         categories: ["all", "home-page"]
